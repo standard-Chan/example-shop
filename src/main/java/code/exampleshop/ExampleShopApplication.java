d078b66278314b84eaea4d456df64fd6083a8e90
standard-chan <starp321@naver.com>
2026-04-02 22:03:35 +0900

    init

---
+++ src/main/java/code/exampleshop/ExampleShopApplication.java
@@ -0,0 +1,13 @@
+package code.exampleshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleShopApplication.class, args);
+    }
+
+}
